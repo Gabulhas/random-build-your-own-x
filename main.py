9c00b357c7cb806a628f628db964223cbc94cf60
Gabulhas <batata@DESKTOP-31KB4DT.localdomain>
2020-08-17 16:02:18 +0100

    improved text and doesnt break when there are no tutorials for a language

--- main.py
+++ main.py
@@ -4,23 +4,33 @@ import random
 import os
 import urllib.request
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
+plus_symbol = "[\x1b[32m+\x1b[0m]"
+minus_symbol = "[\x1b[31m-\x1b[0m]"
+info_symbol = "[\x1b[33m=\x1b[0m]"
 
 arguments = sys.argv[1:]
 
-if len(arguments) < 2:
-    print("REQUIRED ARGUMENTS: <LANGUAGE>(exp:OCaml, Python) <R(andom) or A(ll)>")
-    exit(1)
-if arguments[1] != "R" and arguments[1] != "A":
-    print("REQUIRED ARGUMENTS: <LANGUAGE>(exp:OCaml, Python) <R(andom) or A(ll)>")
+
+def quit_program():
+    print(info_symbol + " REQUIRED ARGUMENTS: <LANGUAGE>(exp:OCaml, Python) <R(andom) or A(ll)>")
     exit(1)
 
-print("Fetching latest version of https://github.com/danistefanovic/build-your-own-x/blob/master/README.md\n")
+
+if len(arguments) < 2:
+    quit_program()
+if arguments[1] != "R" and arguments[1] != "A":
+    quit_program()
+
+print(
+    info_symbol + "Fetching latest version of https://github.com/danistefanovic/build-your-own-x/blob/master/README"
+                  ".md\n")
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
 url = 'https://raw.githubusercontent.com/danistefanovic/build-your-own-x/master/README.md'
-urllib.request.urlretrieve(url, (dir_path + "README.md"))
+urllib.request.urlretrieve(url, os.path.join(dir_path, "source.txt"))
 
-filepath = "./README.md"
+filepath = "source.txt"
 all_languages = {}
 
 language_regex = re.compile('(?<=\[\*\*)(.*)(?=\*\*)')
@@ -49,9 +59,13 @@ with open(filepath, encoding='utf-8', errors='ignore') as fp:
         totalLines += 1
         line = fp.readline()
 
+if arguments[0] not in all_languages:
+    print(minus_symbol + " Couldn't find any Tutorial for your language")
+    exit(1)
+
 if arguments[1] == "A":
     for line in all_languages[arguments[0]]:
-        print(pretty_print(line))
+        print(plus_symbol + " " + pretty_print(line))
 elif arguments[1] == "R":
     result = all_languages[arguments[0]]
-    print(pretty_print(result[random.randrange(0, len(result))]))
+    print(plus_symbol + " " + pretty_print(result[random.randrange(0, len(result))]))
